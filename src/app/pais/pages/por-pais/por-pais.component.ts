eeb44b4cb950e22d19b78f72e50dc6790d826048
Melania <m_fondevilla@hotmail.com>
2021-10-10 10:37:59 +0200

    formulario por países

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-por-pais',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorPaisComponent implements OnInit {
+export class PorPaisComponent {
+
+  termino: string="";
 
   constructor() { }
 
-  ngOnInit(): void {
+  buscar() {
+    console.log(this.termino);
   }
 
 }
