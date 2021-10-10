3a1049821c3b81810315d690cdc340bc474b46ea
Melania <m_fondevilla@hotmail.com>
2021-10-10 11:42:43 +0200

    rellenar la tabla

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 
 @Component({
@@ -12,6 +13,7 @@ export class PorPaisComponent {
 
   termino: string="";
   hayError: boolean=false;
+  paises: Country[] = [];
 
   constructor(private PaisService: PaisService) { }
 
@@ -19,10 +21,12 @@ export class PorPaisComponent {
     this.hayError = false;
     console.log(this.termino);
     this.PaisService.buscarPais(this.termino)
-      .subscribe(paises=>{        
-        console.log(paises);
+      .subscribe(paises=>{    
+        console.log(paises)    ;
+         this.paises = paises;
       }, (err)=>{
          this.hayError=true;
+         this.paises = [];
       });
   }
 
